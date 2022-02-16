15fff2f51f3a43763764b13095143ca34784c6d1
Adam Migus <adam@migus.org>
2022-02-16 11:43:58 -0500

    Use a self-signed cert.

--- scripts/get_cert.sh
+++ scripts/get_cert.sh
@@ -4,7 +4,7 @@ program=$(basename $0)
 
 DEFAULT_NAMESPACE="default"
 DEFAULT_DIRECTORY=.
-DEFAULT_BITS=2048
+DEFAULT_BITS=4096
 
 usage() {
   cat<<EOF
@@ -74,14 +74,6 @@ test -z "${namespace}" && namespace=${DEFAULT_NAMESPACE}
 
 fqdn="${name}.${namespace}.svc"
 
-kubectl=$(which kubectl)
-
-if test $? -ne 0
-then
-  echo "${program}: unable to locate kubectl executable; exiting"
-  exit 1
-fi
-
 openssl=$(which openssl)
 
 if test $? -ne 0
@@ -92,76 +84,26 @@ fi
 
 cd "${directory}"
 
-"${openssl}" genrsa -out "${name}.key" ${bits} 2> /dev/null
+req_conf=`mktemp -t dsv-req.conf.XXXXXX`
+trap 'rm "${req_conf}"; exit' SIGINT SIGTERM EXIT
 
-csr_conf=`mktemp -t dsv-csr.XXXXXX`
-trap 'rm "${csr_conf}"; exit' SIGINT SIGTERM EXIT
-
-cat >"${csr_conf}"<<EOF
+cat >"${req_conf}"<<EOF
 [req]
-req_extensions = v3_req
-distinguished_name = req_distinguished_name
-[req_distinguished_name]
-[v3_req]
-basicConstraints = CA:FALSE
-keyUsage = digitalSignature, keyEncipherment, nonRepudiation
-extendedKeyUsage = serverAuth
-subjectAltName = @alt_names
-[alt_names]
-DNS.1 = ${name}
-DNS.2 = ${name}.${namespace}
-DNS.3 = ${fqdn}
-DNS.4 = ${fqdn}.cluster.local
-EOF
-"${openssl}" req -new -key "${name}.key" \
-  -subj "/CN=system:node:${fqdn};/O=system:nodes" \
-	-out "${name}.csr" -config "${csr_conf}" > /dev/null
+default_bits       = ${DEFAULT_BITS}
+distinguished_name = dn
+prompt             = no
 
-"${kubectl}" delete csr --ignore-not-found=true "${fqdn}" > /dev/null
-"${kubectl}" create -f - <<EOF > /dev/null
-apiVersion: certificates.k8s.io/v1
-kind: CertificateSigningRequest
-metadata:
-  name: ${fqdn}
-spec:
-  signerName: kubernetes.io/kubelet-serving
-  groups:
-  - system:authenticated
-  request: $(cat "${name}.csr" | base64 | tr -d '\n')
-  usages:
-  - digital signature
-  - key encipherment
-  - server auth
+[dn]
+organizationName   = system:nodes
+commonName         = system:node:${fqdn}
+
+[ext]
+subjectAltName     = @sans
+
+[sans]
+DNS.1              = ${name}
+DNS.2              = ${fqdn}
 EOF
 
-retries=0
-while :; do
-  "${kubectl}" get csr "${fqdn}" > /dev/null
-  test "$?" -eq 0 && break
-  if test $retries -gt 9
-  then
-    echo "no csr after ${retries} seconds; exiting"
-    exit 1
-  else
-    let retries=retries+1
-  fi
-  sleep 1
-done
-
-"${kubectl}" certificate approve "${fqdn}" > /dev/null
-
-retries=0
-while :; do
-  cert=$("${kubectl}" get csr ${fqdn} -o jsonpath='{.status.certificate}')
-  test -n "${cert}" && break
-  if test $retries -gt 9
-  then
-    echo "no certificate after ${retries} seconds; exiting"
-    exit 1
-  else
-    let retries=retries+1
-  fi
-  sleep 1
-done
-
-echo ${cert} | "${openssl}" base64 -d -A -out "${name}.pem"
+"${openssl}" req -new -x509 -sha256 -nodes -keyout ${name}.key -days $((365*5))\
+  -extensions ext -out ${name}.pem -config $req_conf 2>/dev/null
