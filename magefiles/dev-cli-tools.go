b43aa3959eec2404cafd9a01ac002683441d6cb7
Sheldon Hull <sheldonhull@users.noreply.github.com>
2022-09-13 16:25:17 -0500

    foo

--- magefiles/dev-cli-tools.go
+++ magefiles/dev-cli-tools.go
@@ -2,7 +2,6 @@ package main
 
 // tools is a list of tools that are installed as binaries for development usage.
 // This list gets installed to go bin directory once `mage init` is run.
-// This is for binaries that need to be invoked as cli tools, not packages.
 var toolList = []string{ //nolint:gochecknoglobals // ok to be global for tooling setup
 	"github.com/golangci/golangci-lint/cmd/golangci-lint@latest",
 	"mvdan.cc/gofumpt@latest",
