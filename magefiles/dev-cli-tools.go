89654846d6ad1ab52c0096c05f94f05872c5c820
sheldonhull <sheldonhull@users.noreply.github.com>
2022-10-24 16:37:32 -0500

    chore: 🎉 align project tooling configuration
    - Align and update all trunk configurations.
    - Include go vendoring on go.mod change.
    - Align markdown formatting.
    - Align vscode default settings.
    - Update devcontainer with newest working configuration on ubuntu base image and using aqua to simplify dev tooling.
    - Align golangci-lint configuration among repos (cleaned up and stripped out all the noisy comments).
    - Align changie configuration for better changelog and release note generation.
    - Improve performance of mage by removing needless tool installation and upgrade to `v1` release.
    
    AB#460170

--- magefiles/dev-cli-tools.go
+++ magefiles/dev-cli-tools.go
@@ -1,7 +1,5 @@
 package main
 
-// tools is a list of tools that are installed as binaries for development usage.
+// ToolList is a list of tools that are installed as binaries for development usage.
 // This list gets installed to go bin directory once `mage init` is run.
-var toolList = []string{ //nolint:gochecknoglobals // ok to be global for tooling setup
-
-}
+var ToolList = []string{}
